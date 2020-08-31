bb809ebe00385f288833054fe5fce89f59e0a740
Arvan Apriyana <40161320+nako48@users.noreply.github.com>
2020-08-31 10:29:29 +0700

    Update ip.sh

--- ip.sh
+++ ip.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 color(){
-merah='\e[91m'
-cyan='\e[96m'
-kuning='\e[93m'
-oren='\033[0;33m' 
-margenta='\e[95m'
-biru='\e[94m'
-ijo="\e[92m"
-putih="\e[97m"
-normal='\033[0m'
-bold='\e[1m'
-labelmerah='\e[41m'
-labelijo='\e[42m'
-labelkuning='\e[43m'    
+	merah='\e[91m'
+	cyan='\e[96m'
+	kuning='\e[93m'
+	oren='\033[0;33m' 
+	margenta='\e[95m'
+	biru='\e[94m'
+	ijo="\e[92m"
+	putih="\e[97m"
+	normal='\033[0m'
+	bold='\e[1m'
+	labelmerah='\e[41m'
+	labelijo='\e[42m'
+	labelkuning='\e[43m'    
 }
 cat << "EOF"
                       .".
@@ -39,7 +39,6 @@ read -p "Mau Berapa IP ? : " limit
 looping=1
 while [ $looping -le $limit ]
 do
-	echo $((RANDOM%255)).$((RANDOM%255)).$((RANDOM%255)).$((RANDOM%255))>>ips.txt
 	echo $((RANDOM%256)).$((RANDOM%256)).$((RANDOM%256)).$((RANDOM%256))>>ips.txt
 	((looping++))
 done
@@ -47,34 +46,36 @@ port80(){
 	color
 	# port=('80')
 	# randport=${port[$RANDOM % ${#port[@]}]}
-	checkip=$(curl -skL --connect-timeout 15 --max-time 15 "https://networkappers.com/api/port.php?ip=$1&port=80" \
+	checkip=$(curl -skL --connect-timeout 15 --max-time 15 "http://agendaxls.eu/css/openport.php?host=${1}&port=80" \
 	-H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0' \
-	-H 'Accept: application/json, text/javascript, */*; q=0.01' \
+	-H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' \
 	-H 'Accept-Language: id,en-US;q=0.7,en;q=0.3' \
-	-H 'X-Requested-With: XMLHttpRequest' \
 	-H 'Connection: keep-alive' \
-	-H 'Referer: https://networkappers.com/tools/open-port-checker')
-	if [[ $checkip =~ "open" ]]; then
+	-H 'Upgrade-Insecure-Requests: 1' )
+	if [[ $checkip =~ "http OPEN" ]]; then
 		printf "${labelijo}-- LIVE --${normal} ${bold} ${1}:80\n"
 		echo "$1">> result.txt
-	else
+	elif [[ $checkip =~ "BAD" ]]; then
 		printf "${labelmerah}-- DEAD --${normal} ${bold} ${1}\n"
+	else
+		printf "${labelmerah}-- BAD --${normal} ${bold} ${1}\n"
 	fi
 }
 port443(){
 	port80 $1
-	checkip=$(curl -skL --connect-timeout 15 --max-time 15 "https://networkappers.com/api/port.php?ip=$1&port=443" \
+	checkip=$(curl -skL --connect-timeout 15 --max-time 15 "http://agendaxls.eu/css/openport.php?host=${1}&port=443" \
 	-H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0' \
-	-H 'Accept: application/json, text/javascript, */*; q=0.01' \
+	-H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' \
 	-H 'Accept-Language: id,en-US;q=0.7,en;q=0.3' \
-	-H 'X-Requested-With: XMLHttpRequest' \
 	-H 'Connection: keep-alive' \
-	-H 'Referer: https://networkappers.com/tools/open-port-checker')
-	if [[ $checkip =~ "open" ]]; then
-		printf "${labelijo}-- LIVE --${normal} ${bold} ${1}:443\n"
+	-H 'Upgrade-Insecure-Requests: 1' )
+	if [[ $checkip =~ "http OPEN" ]]; then
+		printf "${labelijo}-- LIVE --${normal} ${bold} ${1}:80\n"
 		echo "$1">> result.txt
-	else
+	elif [[ $checkip =~ "BAD" ]]; then
 		printf "${labelmerah}-- DEAD --${normal} ${bold} ${1}\n"
+	else
+		printf "${labelmerah}-- BAD --${normal} ${bold} ${1}\n"
 	fi
 }
 IFS=$'\r\n' GLOBIGNORE='*' command eval 'bacot=($(cat ips.txt))'
@@ -87,7 +88,3 @@ for (( i = 0; i <"${#bacot[@]}"; i++ )); do
 	port443 ${ipx} &
 done
 wait
-sort result.txt | uniq >> iplive.txt
-filter_total=$(cat iplive.txt | wc -l)
-printf "Done Filter => $filter_total"
-rm -rf result.txt
